4c747262ab9a58a5a0ad4436f4fd5a26c8afc753
Omar <omarpr@gmail.com>
2018-01-16 16:59:29 -0500

    Save point.

--- lecture5/oop.php
+++ lecture5/oop.php
@@ -16,20 +16,80 @@ class Person {
     public function getFullName() {
         return "$this->firstName $this->lastName1 $this->lastName2";
     }
+    
+    public function getCalculatedAge() {
+        static $age = null;
+        
+        if (is_null($age)) {
+            $t = strtotime($this->dateOfBirth);
+            $diff = time() - $t;
+            $year = 60 * 60 * 24 * 365;
+            $age = floor($diff / $year);
+            
+            echo 'I calculated it!<br />';
+        }
+        
+        return $age;
+    }
 }
 
 $a = new Person();
 $a->firstName = 'Omar';
 $a->lastName1 = 'Soto';
 $a->lastName2 = 'Fortuno';
-$a->dateOfBirth = '19/Feb/1989';
+$a->dateOfBirth = '2/19/1989';
 
 echo '<pre>' . print_r($a, true) . '</pre>';
 
-$b = new Person('Omar', 'Soto', 'Fortuno', '19/Feb/1989');
+$b = new Person('Omar', 'Soto', 'Fortuno', '2/19/1989');
 
 echo '<pre>' . print_r($b, true) . '</pre>';
 
 echo $b->getFullName() . '<br />';
+echo 'Years: ' . $b->getCalculatedAge() . '<br />';
+echo 'Years: ' . $b->getCalculatedAge() . '<br />';
+echo 'Years: ' . $b->getCalculatedAge() . '<br />';
+echo 'Years: ' . $b->getCalculatedAge() . '<br />';
+echo 'Years: ' . $b->getCalculatedAge() . '<br />';
+echo 'Years: ' . $b->getCalculatedAge() . '<br />';
+
+class Student extends Person {
+    public $grades = [];
+    
+    public function addGrade($grade) {
+        $this->grades[] = $grade;
+    }
+    
+    public function getCalculatedGPA() {
+        $ph = 0;
+        
+        foreach ($this->grades as $grade) {
+            if ($grade == 'A') {
+                $ph += 4;
+            } elseif ($grade == 'B') {
+                $ph += 3;
+            } elseif ($grade == 'C') {
+                $ph += 2;
+            } elseif ($grade == 'D') {
+                $ph += 1;
+            }
+        }
+        
+        return round($ph / count($this->grades), 2);
+    }
+}
+
+$c = new Student('Omar', 'Soto', 'Fortuno', '2/19/1989');
+
+$c->addGrade('A');
+$c->addGrade('A');
+$c->addGrade('B');
+$c->addGrade('C'); // SICI 4997
+
+echo '<pre>' . print_r($c, true) . '</pre>';
+
+echo $c->getFullName() . '<br />';
+echo 'Years: ' . $c->getCalculatedAge() . '<br />';
+echo 'GPA: ' . $c->getCalculatedGPA() . '<br />';
 
 ?>
