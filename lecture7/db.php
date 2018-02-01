fbe32d4ada462d966bd4dc2ed9815da062a82a91
Omar <omarpr@gmail.com>
2018-02-01 15:51:24 -0500

    Save point.

--- lecture7/db.php
+++ lecture7/db.php
@@ -8,7 +8,14 @@ $dbPassword = 'sici4997';
 $db = new PDO("mysql:host=$dbHostname;dbname=$dbDatabase;charset=utf8", $dbUsername, $dbPassword);
 
 //queryOne();
-queryTwo();
+//queryTwo();
+//queryThree();
+
+$records = getResultFromSQL('SELECT * FROM user');
+echo '<pre>' . print_r($records, true) . '</pre>';
+
+$records = getResultFromSQL('SELECT * FROM user WHERE username = ?', ['ctk']);
+echo '<pre>' . print_r($records, true) . '</pre>';
 
 function queryOne() {
     global $db;
@@ -23,12 +30,39 @@ function queryTwo() {
     global $db;
     //$id = 1;
     // SQL Injection Example
-    $id = '1; truncate table user;';
+    //$id = '1; truncate table user;';
+    $id = 1;
     $sql = 'SELECT * FROM user WHERE id = ' . $id;
     
     foreach ($db->query($sql) as $rec) {
         echo '<pre>' . print_r($rec, true) . '</pre>';
     }
+}
+
+function queryThree() {
+    global $db;
+    
+    // SQL Injection Example
+    $id = '1; truncate table user;';
+    //$id = 1;
+    
+    $sql = 'SELECT * FROM user WHERE id = ?';
+    $stmt = $db->prepare($sql);
+    
+    $stmt->execute([$id]);
+    
+    foreach ($stmt->fetchAll() as $rec) {
+        echo '<pre>' . print_r($rec, true) . '</pre>';
+    }
+}
+
+function getResultFromSQL($sql, $values = []) {
+    global $db;
+    
+    $stmt = $db->prepare($sql);
+    $stmt->execute($values);
+    
+    return $stmt->fetchAll();
 }
 
 ?>
